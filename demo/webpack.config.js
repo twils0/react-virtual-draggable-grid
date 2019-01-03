28588da166c157ecd03651079d675ebadc530e96
Tyler Wilson <twils0@protonmail.com>
2019-01-02 21:42:08 -0500

    update webpack config

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -8,69 +8,73 @@ const rootDir = path.resolve(__dirname);
 const nodeModDir = path.resolve(__dirname, 'node_modules');
 const appSrcDir = path.resolve(__dirname, '../src');
 
-const config = {
-  entry: [path.resolve(rootDir, 'index.jsx')],
-  output: {
-    path: rootDir,
-    filename: 'react-virtual-draggable-grid-demo.min.js',
-    libraryTarget: 'umd',
-    library: 'default',
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    historyApiFallback: true,
-    hotOnly: true,
-  },
-  resolve: {
-    modules: [rootDir, appSrcDir, nodeModDir],
-    extensions: ['.js', '.jsx'],
-  },
-  target: 'web',
-  module: {
-    rules: [
-      {
-        test: /\.(jsx?)$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-      {
-        test: /\.html$/,
-        loader: 'html-loader',
-      },
-    ],
-  },
-  optimization: {
-    runtimeChunk: false,
-    splitChunks: {
-      cacheGroups: {
-        vendors: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          filename: 'vendors-demo.min.js',
-          chunks: 'all',
-        },
-      },
+const config = (env) => {
+  const prod = env.prod === 'true';
+
+  return {
+    entry: [path.resolve(rootDir, 'index.jsx')],
+    output: {
+      path: rootDir,
+      // cannot use contenthash with runtime
+      filename: chunkData => `[name].[${chunkData.chunk.name === 'runtime' ? 'hash' : 'contenthash'}]${prod
+          && '.min'}.js`,
+      chunkFilename: `[name].[contenthash]${prod && '.min'}.js`,
     },
-    minimizer: [
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-        uglifyOptions: {
-          compress: false,
-          ecma: 6,
-          mangle: true,
+    devtool: 'inline-source-map',
+    devServer: {
+      historyApiFallback: true,
+      hotOnly: true,
+    },
+    resolve: {
+      modules: [rootDir, appSrcDir, nodeModDir],
+      extensions: ['.js', '.jsx'],
+    },
+    target: 'web',
+    module: {
+      rules: [
+        {
+          test: /\.(jsx?)$/,
+          exclude: /node_modules/,
+          use: 'babel-loader',
         },
+        {
+          test: /\.html$/,
+          loader: 'html-loader',
+        },
+      ],
+    },
+    optimization: {
+      runtimeChunk: 'single',
+      splitChunks: {
+        chunks: 'all',
+        cacheGroups: {
+          vendors: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+          },
+          react: {
+            test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+            name: 'react',
+            priority: 1,
+          },
+        },
+      },
+      minimizer: [
+        new UglifyJsPlugin({
+          cache: true,
+          parallel: true,
+        }),
+      ],
+    },
+    plugins: [
+      new webpack.optimize.OccurrenceOrderPlugin(),
+      new webpack.NamedModulesPlugin(),
+      new webpack.HotModuleReplacementPlugin(),
+      new HtmlWebpackPlugin({
+        template: path.resolve(rootDir, './src/index.html'),
       }),
     ],
-  },
-  plugins: [
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({
-      template: path.resolve(rootDir, './src/index.html'),
-    }),
-  ],
+  };
 };
 
 module.exports = config;
--- webpack.config.js
+++ webpack.config.js
@@ -10,76 +10,78 @@ const binDir = path.resolve(__dirname, 'bin');
 const srcDir = path.resolve(__dirname, 'src');
 const nodeModDir = path.resolve(__dirname, 'node_modules');
 
-const config = {
-  entry: [path.resolve(srcDir, 'VirtualDraggableGrid.jsx')],
-  output: {
-    path: binDir,
-    filename: 'react-virtual-draggable-grid.min.js',
-    libraryTarget: 'umd',
-    library: 'default',
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    historyApiFallback: true,
-    hotOnly: true,
-  },
-  resolve: {
-    modules: [rootDir, nodeModDir],
-    extensions: ['.js', '.jsx'],
-  },
-  target: 'web',
-  externals: {
-    'prop-types': {
-      commonjs: 'prop-types',
-      commonjs2: 'prop-types',
-      amd: 'prop-types',
-      root: 'PropTypes',
+const config = (env) => {
+  const prod = env.prod === 'true';
+
+  return {
+    entry: [path.resolve(srcDir, 'VirtualDraggableGrid.jsx')],
+    output: {
+      path: binDir,
+      // cannot use contenthash with runtime
+      filename: chunkData => `[name].[${chunkData.chunk.name === 'runtime' ? 'hash' : 'contenthash'}]${prod
+          && '.min'}.js`,
+      chunkFilename: `[name].[contenthash]${prod && '.min'}.js`,
+      libraryTarget: 'umd',
+      library: 'default',
     },
-    react: {
-      commonjs: 'react',
-      commonjs2: 'react',
-      amd: 'react',
-      root: 'React',
+    devtool: 'inline-source-map',
+    devServer: {
+      historyApiFallback: true,
+      hotOnly: true,
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(jsx?)$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
+    resolve: {
+      modules: [rootDir, nodeModDir],
+      extensions: ['.js', '.jsx'],
+    },
+    target: 'web',
+    externals: {
+      'prop-types': {
+        commonjs: 'prop-types',
+        commonjs2: 'prop-types',
+        amd: 'prop-types',
+        root: 'PropTypes',
       },
-    ],
-  },
-  optimization: {
-    runtimeChunk: false,
-    splitChunks: {
-      chunks: 'all',
-      cacheGroups: {
-        vendors: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-        },
+      react: {
+        commonjs: 'react',
+        commonjs2: 'react',
+        amd: 'react',
+        root: 'React',
       },
     },
-    minimizer: [
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-        uglifyOptions: {
-          compress: false,
-          ecma: 6,
-          mangle: true,
+    module: {
+      rules: [
+        {
+          test: /\.(jsx?)$/,
+          exclude: /node_modules/,
+          use: 'babel-loader',
         },
-      }),
+      ],
+    },
+    optimization: {
+      runtimeChunk: 'single',
+      splitChunks: {
+        chunks: 'all',
+        cacheGroups: {
+          vendors: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+          },
+        },
+      },
+      minimizer: [
+        new UglifyJsPlugin({
+          cache: true,
+          parallel: true,
+        }),
+      ],
+    },
+    plugins: [
+      new CleanWebpackPlugin([binDir]),
+      new webpack.optimize.OccurrenceOrderPlugin(),
+      new webpack.NamedModulesPlugin(),
+      new BundleAnalyzer(),
     ],
-  },
-  plugins: [
-    new CleanWebpackPlugin([binDir]),
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.NamedModulesPlugin(),
-    new BundleAnalyzer(),
-  ],
+  };
 };
 
 module.exports = config;
