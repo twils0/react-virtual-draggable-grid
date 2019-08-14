919058b409638c7a1ace70b4c06b6a03ca80f7aa
twils0 <twils0@protonmail.com>
2019-08-13 20:27:17 -0500

    move test npm scripts and test configs to root dir

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,23 @@
 const config = {
   presets: ['@babel/preset-env', '@babel/preset-react'],
   plugins: [
-    'dynamic-import-node',
     '@babel/plugin-syntax-dynamic-import',
     '@babel/plugin-transform-react-jsx-source',
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-object-rest-spread',
   ],
+  env: {
+    test: {
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+      plugins: [
+        'babel-plugin-dynamic-import-node',
+        '@babel/plugin-syntax-dynamic-import',
+        '@babel/plugin-transform-react-jsx-source',
+        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-proposal-object-rest-spread',
+      ],
+    },
+  },
 };
 
 module.exports = config;
